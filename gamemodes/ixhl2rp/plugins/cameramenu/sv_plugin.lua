3fe8e08ff3e58f9e8b5f6bff27040c803cc40164
krrkkk <166711165+krrkkk@users.noreply.github.com>
2025-05-16 23:50:33 +0300

    Update sv_plugin.lua

--- gamemodes/ixhl2rp/plugins/cameramenu/sv_plugin.lua
+++ gamemodes/ixhl2rp/plugins/cameramenu/sv_plugin.lua
@@ -5,80 +5,119 @@ util.AddNetworkString("ixCameraMenuPoints")
 util.AddNetworkString("ixRequestCameraPoints")
 util.AddNetworkString("ixAddCameraPoint")
 util.AddNetworkString("ixClearCameraPoints")
+util.AddNetworkString("ixUpdateCameraPointSpeed")
 
+-- Создаём таблицу в SQLite при запуске
+function PLUGIN:Initialize()
+    sql.Query([[
+        CREATE TABLE IF NOT EXISTS camera_points (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            map TEXT,
+            pos_x REAL,
+            pos_y REAL,
+            pos_z REAL,
+            ang_p REAL,
+            ang_y REAL,
+            ang_r REAL,
+            speed REAL
+        )
+    ]])
+    self:LoadCameraPoints()
+end
+
+-- Загружаем точки из SQLite
+function PLUGIN:LoadCameraPoints()
+    local map = game.GetMap()
+    self.cameraData[map] = {}
+
+    local data = sql.Query("SELECT * FROM camera_points WHERE map = " .. sql.SQLStr(map))
+    if data then
+        for _, row in ipairs(data) do
+            table.insert(self.cameraData[map], {
+                pos = Vector(tonumber(row.pos_x), tonumber(row.pos_y), tonumber(row.pos_z)),
+                ang = Angle(tonumber(row.ang_p), tonumber(row.ang_y), tonumber(row.ang_r)),
+                speed = tonumber(row.speed)
+            })
+        end
+    end
+end
+
+-- Сохраняем точку в SQLite
+function PLUGIN:SaveCameraPoint(map, pos, ang, speed)
+    sql.Query(string.format([[
+        INSERT INTO camera_points (map, pos_x, pos_y, pos_z, ang_p, ang_y, ang_r, speed)
+        VALUES (%s, %f, %f, %f, %f, %f, %f, %f)
+    ]],
+        sql.SQLStr(map),
+        pos.x, pos.y, pos.z,
+        ang.p, ang.y, ang.r,
+        speed
+    ))
+end
+
+-- Удаляем точки из SQLite
+function PLUGIN:ClearCameraPointsInDB(map)
+    sql.Query("DELETE FROM camera_points WHERE map = " .. sql.SQLStr(map))
+end
+
+-- Команда добавления точки
 ix.command.Add("AddCameraPoint", {
     adminOnly = true,
     arguments = {ix.type.number},
     OnRun = function(self, client, speed)
         local map = game.GetMap()
+        local pos = client:GetPos() + Vector(0, 0, 64)
+        local ang = client:EyeAngles()
 
-        if type(PLUGIN.cameraData) ~= "table" then
-            PLUGIN.cameraData = {}
-        end
+        PLUGIN.cameraData[map] = PLUGIN.cameraData[map] or {}
+        table.insert(PLUGIN.cameraData[map], { pos = pos, ang = ang, speed = speed })
+        PLUGIN:SaveCameraPoint(map, pos, ang, speed)
 
-        if type(PLUGIN.cameraData[map]) ~= "table" then
-            PLUGIN.cameraData[map] = {}
-        end
-
-        table.insert(PLUGIN.cameraData[map], {
-            pos = client:GetPos() + Vector(0,0,64),
-            ang = client:EyeAngles(),
-            speed = speed
-        })
-
-        client:Notify("Точка камеры добавлена для карты "..map)
+        client:Notify("Точка камеры добавлена для карты " .. map)
     end
 })
 
+-- Команда очистки точек
 ix.command.Add("ClearCameraPoints", {
     adminOnly = true,
     OnRun = function(self, client)
         local map = game.GetMap()
-
-        if type(PLUGIN.cameraData) ~= "table" then
-            PLUGIN.cameraData = {}
-        end
-
         PLUGIN.cameraData[map] = {}
+        PLUGIN:ClearCameraPointsInDB(map)
 
-        client:Notify("Точки камеры очищены для карты "..map)
+        client:Notify("Точки камеры очищены для карты " .. map)
+
+        for _, v in ipairs(player.GetAll()) do
+            net.Start("ixCameraMenuPoints")
+                net.WriteTable({})
+            net.Send(v)
+        end
     end
 })
 
-net.Receive("ixRequestCameraPoints", function(len, ply)
+-- Отправка точек клиенту
+net.Receive("ixRequestCameraPoints", function(_, ply)
     local map = game.GetMap()
-
-    if type(PLUGIN.cameraData) ~= "table" then
-        PLUGIN.cameraData = {}
-    end
-
     net.Start("ixCameraMenuPoints")
         net.WriteTable(PLUGIN.cameraData[map] or {})
     net.Send(ply)
 end)
 
-net.Receive("ixAddCameraPoint", function(len, ply)
+-- Добавление точки с клиента (через VGUI)
+net.Receive("ixAddCameraPoint", function(_, ply)
     if not ply:IsAdmin() then return end
 
     local map = game.GetMap()
+    local pos = ply:GetPos() + Vector(0, 0, 64)
+    local ang = ply:EyeAngles()
+    local speed = 3
 
-    if type(PLUGIN.cameraData) ~= "table" then
-        PLUGIN.cameraData = {}
-    end
+    PLUGIN.cameraData[map] = PLUGIN.cameraData[map] or {}
+    table.insert(PLUGIN.cameraData[map], { pos = pos, ang = ang, speed = speed })
+    PLUGIN:SaveCameraPoint(map, pos, ang, speed)
 
-    if type(PLUGIN.cameraData[map]) ~= "table" then
-        PLUGIN.cameraData[map] = {}
-    end
+    ply:Notify("Точка камеры добавлена для карты " .. map)
 
-    table.insert(PLUGIN.cameraData[map], {
-        pos = ply:GetPos() + Vector(0,0,64),
-        ang = ply:EyeAngles(),
-        speed = 3 -- фиксированная скорость для добавления через VGUI
-    })
-
-    ply:Notify("Точка камеры добавлена для карты "..map)
-
-    -- Обновляем клиентам (если нужно)
     for _, v in ipairs(player.GetAll()) do
         net.Start("ixCameraMenuPoints")
             net.WriteTable(PLUGIN.cameraData[map])
@@ -86,52 +125,28 @@ net.Receive("ixAddCameraPoint", function(len, ply)
     end
 end)
 
-net.Receive("ixClearCameraPoints", function(len, ply)
-    if not ply:IsAdmin() then return end
-
-    local map = game.GetMap()
-
-    if type(PLUGIN.cameraData) ~= "table" then
-        PLUGIN.cameraData = {}
-    end
-
-    PLUGIN.cameraData[map] = {}
-
-    ply:Notify("Точки камеры очищены для карты "..map)
-
-    for _, v in ipairs(player.GetAll()) do
-        net.Start("ixCameraMenuPoints")
-            net.WriteTable({})
-        net.Send(v)
-    end
-end)
-
-util.AddNetworkString("ixUpdateCameraPointSpeed")
-
-net.Receive("ixUpdateCameraPointSpeed", function(len, ply)
+-- Обновление скорости точки
+net.Receive("ixUpdateCameraPointSpeed", function(_, ply)
     if not ply:IsAdmin() then return end
 
     local index = net.ReadInt(16)
     local newSpeed = net.ReadFloat()
-
     local map = game.GetMap()
 
-    if type(PLUGIN.cameraData) ~= "table" then
-        PLUGIN.cameraData = {}
-    end
-
-    if type(PLUGIN.cameraData[map]) ~= "table" then
-        PLUGIN.cameraData[map] = {}
-    end
-
-    if PLUGIN.cameraData[map][index] then
-        PLUGIN.cameraData[map][index].speed = newSpeed
+    local data = PLUGIN.cameraData[map]
+    if data and data[index] then
+        data[index].speed = newSpeed
         ply:Notify("Скорость точки #" .. index .. " обновлена")
 
-        -- Рассылаем обновлённые данные всем игрокам
+        -- Применяем обновление в БД (по-простому: очистим и заново сохраним)
+        PLUGIN:ClearCameraPointsInDB(map)
+        for _, point in ipairs(data) do
+            PLUGIN:SaveCameraPoint(map, point.pos, point.ang, point.speed)
+        end
+
         for _, v in ipairs(player.GetAll()) do
             net.Start("ixCameraMenuPoints")
-                net.WriteTable(PLUGIN.cameraData[map])
+                net.WriteTable(data)
             net.Send(v)
         end
     else
@@ -139,3 +154,5 @@ net.Receive("ixUpdateCameraPointSpeed", function(len, ply)
     end
 end)
 
+-- Вызов инициализации
+PLUGIN:Initialize()
